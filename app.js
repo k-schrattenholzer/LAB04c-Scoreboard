f74f4274dae768999f407c29ba284a1bd3a4409a
‘Katie <ktschr11@gmail.comgit config --global user.name k-schrattenholzergit config --global user.email ktschr11@gmail.com>
2021-08-13 13:51:11 -0700

    add button, counter and score elements

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // import functions and grab DOM elements
-
+const ratButton = document.getElementById ('rat-button');
+const ratScore = document.getElementById ('rat-score');
+const possumButton = document.getElementById ('possum-button');
+const possumScore = document.getElementById ('possum-score');
 // initialize state
 
+let points = 0;
+
 // set event listeners 
+ratButton.addEventListener('click', () => {
+    points = points + 7;
+    ratScore.textContent = points;
+});
+
+possumButton.addEventListener('click', () => {
+    points = points + 7;
+    possumScore.textContent = points;
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
